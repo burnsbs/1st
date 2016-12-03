a83cb1927b79e07774c65d06982911cc566a276a
burnsbs <burnsb@gmail.com>
2016-12-03 00:12:43 -0500

    Add files via upload

---
+++ gradle/wrapper/src/main/java/com/example/brian/myapplication2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.brian.myapplication2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
